acf27ebd0fd71e8b771c568663f96e0491feb67a
Brian McHugh <125cedarstreet@gmail.com>
2020-08-27 21:26:14 -0500

    Remove path from server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const port = 5000;
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
+app.use(express.static("../client/dist"));
 
 app.listen(port, () => {
   console.log(`Nice job, you're connected!`);
