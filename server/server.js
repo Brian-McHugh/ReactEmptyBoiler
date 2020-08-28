dc5b3d31cab72e2df6f0874cae7db393f94155d4
Brian McHugh <125cedarstreet@gmail.com>
2020-08-27 21:25:13 -0500

    Remove path from server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = 3000;
 const { postTask, getTasks, deleteTask } = require('../db/query');
 
-app.use(express.static(path.join(__dirname, '../client/dist/')));
+app.use(express.static('../client/dist/'));
 app.use(express.json());
 
 app.get('/tasks', (req, res) => {
